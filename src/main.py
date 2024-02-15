1885394048c4b3ca5ffc66bbdfcafe02372d3490
Ben Gosney <bengosney@googlemail.com>
2024-02-15 12:56:38 +0000

    Add typer cli support

--- src/main.py
+++ src/main.py
@@ -1,43 +1,56 @@
-# Standard Library
+# Third Party
+import typer
 
 # First Party
 from grid import Grid
 from tile import Tile
 
-tileset = [
-    Tile("═", up="aaa", right="aba", down="aaa", left="aba"),
-    Tile("║", up="aba", right="aaa", down="aba", left="aaa"),
-    Tile("╔", up="aaa", right="aba", down="aba", left="aaa"),
-    Tile("╚", up="aba", right="aba", down="aaa", left="aaa"),
-    Tile("╗", up="aaa", right="aaa", down="aba", left="aba"),
-    Tile("╝", up="aba", right="aaa", down="aaa", left="aba"),
-    Tile("╬", up="aba", right="aba", down="aba", left="aba"),
-    Tile("╠", up="aba", right="aba", down="aba", left="aaa"),
-    Tile("╣", up="aba", right="aaa", down="aba", left="aba"),
-    Tile("╦", up="aaa", right="aba", down="aba", left="aba"),
-    Tile("╩", up="aba", right="aba", down="aaa", left="aba"),
-    Tile(" ", up="aaa", right="aaa", down="aaa", left="aaa", weight=100),
-    Tile("🯅", up="aaa", right="aaa", down="aaa", left="aaa", weight=1),
-    Tile("─", up="aaa", right="aca", down="aaa", left="aca"),
-    Tile("│", up="aca", right="aaa", down="aca", left="aaa"),
-    Tile("┌", up="aaa", right="aca", down="aca", left="aaa"),
-    Tile("└", up="aca", right="aca", down="aaa", left="aaa"),
-    Tile("┐", up="aaa", right="aaa", down="aca", left="aca"),
-    Tile("┘", up="aca", right="aaa", down="aaa", left="aca"),
-    Tile("┼", up="aca", right="aca", down="aca", left="aca"),
-    Tile("├", up="aca", right="aca", down="aca", left="aaa"),
-    Tile("┤", up="aca", right="aaa", down="aca", left="aca"),
-    Tile("┬", up="aaa", right="aca", down="aca", left="aca"),
-    Tile("┴", up="aca", right="aca", down="aaa", left="aca"),
-    Tile("╤", up="aaa", right="aba", down="aca", left="aba"),
-    Tile("╧", up="aca", right="aba", down="aaa", left="aba"),
-    Tile("╟", up="aba", right="aca", down="aba", left="aaa"),
-    Tile("╢", up="aba", right="aaa", down="aba", left="aca"),
-    Tile("╥", up="aaa", right="aca", down="aba", left="aca"),
-    Tile("╨", up="aba", right="aca", down="aaa", left="aca"),
-    Tile("╞", up="aca", right="aba", down="aca", left="aaa"),
-    Tile("╡", up="aca", right="aaa", down="aca", left="aba"),
+tilesets = [
+    [
+        Tile("═", up="aaa", right="aba", down="aaa", left="aba"),
+        Tile("║", up="aba", right="aaa", down="aba", left="aaa"),
+        Tile("╔", up="aaa", right="aba", down="aba", left="aaa"),
+        Tile("╚", up="aba", right="aba", down="aaa", left="aaa"),
+        Tile("╗", up="aaa", right="aaa", down="aba", left="aba"),
+        Tile("╝", up="aba", right="aaa", down="aaa", left="aba"),
+        Tile("╬", up="aba", right="aba", down="aba", left="aba"),
+        Tile("╠", up="aba", right="aba", down="aba", left="aaa"),
+        Tile("╣", up="aba", right="aaa", down="aba", left="aba"),
+        Tile("╦", up="aaa", right="aba", down="aba", left="aba"),
+        Tile("╩", up="aba", right="aba", down="aaa", left="aba"),
+        Tile(" ", up="aaa", right="aaa", down="aaa", left="aaa", weight=1000),
+        Tile("🯅", up="aaa", right="aaa", down="aaa", left="aaa", weight=1),
+    ],
+    [
+        Tile("─", up="aaa", right="aca", down="aaa", left="aca"),
+        Tile("│", up="aca", right="aaa", down="aca", left="aaa"),
+        Tile("┌", up="aaa", right="aca", down="aca", left="aaa"),
+        Tile("└", up="aca", right="aca", down="aaa", left="aaa"),
+        Tile("┐", up="aaa", right="aaa", down="aca", left="aca"),
+        Tile("┘", up="aca", right="aaa", down="aaa", left="aca"),
+        Tile("┼", up="aca", right="aca", down="aca", left="aca"),
+        Tile("├", up="aca", right="aca", down="aca", left="aaa"),
+        Tile("┤", up="aca", right="aaa", down="aca", left="aca"),
+        Tile("┬", up="aaa", right="aca", down="aca", left="aca"),
+        Tile("┴", up="aca", right="aca", down="aaa", left="aca"),
+        Tile("╤", up="aaa", right="aba", down="aca", left="aba"),
+        Tile("╧", up="aca", right="aba", down="aaa", left="aba"),
+        Tile("╟", up="aba", right="aca", down="aba", left="aaa"),
+        Tile("╢", up="aba", right="aaa", down="aba", left="aca"),
+        Tile("╥", up="aaa", right="aca", down="aba", left="aca"),
+        Tile("╨", up="aba", right="aca", down="aaa", left="aca"),
+        Tile("╞", up="aca", right="aba", down="aca", left="aaa"),
+        Tile("╡", up="aca", right="aaa", down="aca", left="aba"),
+    ],
 ]
 
-grid = Grid(width=100, height=40, tiles=tileset)
-grid.collapse(_print=True)
+
+def main(progress: bool = True, width: int = 100, height: int = 40, full_tileset: bool = True):
+    tileset = tilesets[0] + tilesets[1] if full_tileset else tilesets[0]
+
+    grid = Grid(width=width, height=height, tiles=tileset)
+    grid.collapse(_print=progress)
+
+
+if __name__ == "__main__":
+    typer.run(main)
